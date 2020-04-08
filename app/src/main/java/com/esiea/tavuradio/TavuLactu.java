5aad15fb4161c113b6dbfd0cd96662115ae6a3c3
Martin92350 <martin-noel@outlook.fr>
2020-04-09 00:40:54 +0200

    [AddList] Add a List

--- app/src/main/java/com/esiea/tavuradio/TavuLactu.java
+++ app/src/main/java/com/esiea/tavuradio/TavuLactu.java
@@ -1,6 +1,8 @@
 package com.esiea.tavuradio;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -8,9 +10,16 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class TavuLactu extends AppCompatActivity {
 
     private Button retour ;
+
+    private RecyclerView recyclerView;
+    private TavuList mAdapter;
+    private RecyclerView.LayoutManager layoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +27,23 @@ public class TavuLactu extends AppCompatActivity {
         setContentView(R.layout.activity_tavu);
 
         this.retour = (Button) findViewById(R.id.retour);
+
+        recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+
+        recyclerView.setHasFixedSize(true);
+
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+
+        List<String> input = new ArrayList<>();
+        for (int i = 0; i < 100; i++) {
+            input.add("Test" + i);
+        }
+
+        mAdapter = new TavuList(input);
+        recyclerView.setAdapter(mAdapter);
+
+
 
         retour.setOnClickListener(new View.OnClickListener() {
             @Override
