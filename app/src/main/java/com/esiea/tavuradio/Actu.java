eef2efee349226274370748b657f8bf53a781139
Martin92350 <martin-noel@outlook.fr>
2020-04-10 21:21:34 +0200

    [serverCall] add a server call and an access to data

---
+++ app/src/main/java/com/esiea/tavuradio/Actu.java
@@ -0,0 +1,15 @@
+package com.esiea.tavuradio;
+
+public class Actu {
+
+    private String name ;
+    private String url ;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+}
--- app/src/main/java/com/esiea/tavuradio/TavuLactu.java
+++ app/src/main/java/com/esiea/tavuradio/TavuLactu.java
@@ -9,9 +9,19 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class TavuLactu extends AppCompatActivity {
 
@@ -28,22 +38,7 @@ public class TavuLactu extends AppCompatActivity {
 
         this.retour = (Button) findViewById(R.id.retour);
 
-        recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
-
-        recyclerView.setHasFixedSize(true);
-
-        layoutManager = new LinearLayoutManager(this);
-        recyclerView.setLayoutManager(layoutManager);
-
-        List<String> input = new ArrayList<>();
-        for (int i = 0; i < 100; i++) {
-            input.add("Test" + i);
-        }
-
-        mAdapter = new TavuList(input);
-        recyclerView.setAdapter(mAdapter);
-
-
+        makeApiCall();
 
         retour.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -54,5 +49,60 @@ public class TavuLactu extends AppCompatActivity {
             }
         });
 
+    }
+
+    private void showList(List<Actu>actuList) {
+
+        recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+
+        recyclerView.setHasFixedSize(true);
+
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+
+
+
+        mAdapter = new TavuList(actuList);
+        recyclerView.setAdapter(mAdapter);
+    }
+
+    private static final String BASE_URL = "https://pokeapi.co/";
+
+    private void makeApiCall () {
+        Gson gson = new GsonBuilder()
+                .setLenient()
+                .create();
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create(gson))
+                .build();
+
+        ActuApi actuApi = retrofit.create(ActuApi.class);
+
+        Call<RestActuResponse> call = actuApi.getActuResponse();
+        call.enqueue(new Callback<RestActuResponse>() {
+            @Override
+            public void onResponse(Call<RestActuResponse> call, Response<RestActuResponse> response) {
+                if(response.isSuccessful()&&response.body() != null){
+
+                        List<Actu> actuList = response.body().getResults();
+                        showList(actuList);
+
+                }else{
+                    showError();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<RestActuResponse> call, Throwable t) {
+                showError();
+            }
+
+            private void showError() {
+
+                Toast.makeText(getApplicationContext(), "API error", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
