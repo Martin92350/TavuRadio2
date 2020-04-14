113584feb9fac7add5f70289f770582a638184ea
Martin92350 <martin-noel@outlook.fr>
2020-04-14 21:25:53 +0200

    [AddReader] add a player

--- app/src/main/java/com/esiea/tavuradio/MainActivity.java
+++ app/src/main/java/com/esiea/tavuradio/MainActivity.java
@@ -3,19 +3,41 @@ package com.esiea.tavuradio;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import java.io.IOException;
+
 public class MainActivity extends AppCompatActivity {
 
     private Button TavuLactu ;
+    private Button Play ;
+    private MediaPlayer mediaPlayer ;
+
+    boolean prepared = false ;
+    boolean started = false ;
+
+    private String stream =  "https://www.radioking.com/play/tavu-radio";
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         this.TavuLactu = (Button) findViewById(R.id.TavuLactu);
+        this.Play = (Button) findViewById(R.id.Play);
+        Play.setEnabled(false);
+        Play.setText("LOADING");
+
+        mediaPlayer = new MediaPlayer() ;
+        mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+
+        new PlayerTask().execute(stream) ;
 
         TavuLactu.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -25,5 +47,69 @@ public class MainActivity extends AppCompatActivity {
                 finish();
             }
         });
+
+        Play.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if(started){
+                    started = false ;
+                    mediaPlayer.pause();
+                    Play.setText("Play");
+                }else{
+                    started = true ;
+                    mediaPlayer.start();
+                    Play.setText("Pause");
+                }
+
+            }
+        });
+
+    }
+
+     class PlayerTask extends AsyncTask<String, Void, Boolean > {
+        @Override
+        protected Boolean doInBackground(String... strings) {
+
+            try {
+                mediaPlayer.setDataSource(strings[0]);
+                mediaPlayer.prepare();
+                prepared = true ;
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return prepared;
+        }
+
+        @Override
+        protected void onPostExecute(Boolean aBoolean) {
+            super.onPostExecute(aBoolean);
+            Play.setEnabled(true);
+            Play.setText("PLAY");
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if(started){
+            mediaPlayer.pause();
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if(started){
+            mediaPlayer.start();
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if(prepared){
+            mediaPlayer.release();
+        }
     }
 }
