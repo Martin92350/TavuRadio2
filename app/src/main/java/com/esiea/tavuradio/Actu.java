58b281f45ace268bdd5635907e885a3ee34e0631
Martin92350 <martin-noel@outlook.fr>
2020-04-17 17:44:26 +0200

    update the new api

--- app/src/main/java/com/esiea/tavuradio/Actu.java
+++ app/src/main/java/com/esiea/tavuradio/Actu.java
@@ -2,13 +2,13 @@ package com.esiea.tavuradio;
 
 public class Actu {
 
-    private String image ;
+    //private String image ;
     private String titre ;
     private String description ;
 
-    public String getImage() {
+   /* public String getImage() {
         return image;
-    }
+    }*/
 
     public String getTitre() {
         return titre;
